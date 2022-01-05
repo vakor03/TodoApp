196a67e82bff4862746801a76b722bcbed6182d6
Vakor <myksv200@gmail.com>
2022-01-05 13:06:34 +0200

    bug with invalid link

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
-import ApolloWrapper from './components/ApolloWrapper';
+import ApolloWrapper from './Components/appolo-wrappers/appolo-wrapper';
 import config from './config.json';
 
 ReactDOM.render(
